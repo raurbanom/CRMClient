738554c14702351b767489c1a6f022dafbd5bdaa
Richar Urbano <richar.urbano@bizagi.com>
2021-11-29 15:34:17 -0500

    Add basic pages

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const Index = () => (
   <div>
     <Layout>
-      <h2>Desde Index</h2>
+      <h1 className="text-2xl text-gray-800 font-light">Client</h1>
     </Layout>
   </div>
 )
---
+++ pages/product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from "../components/Layout";
+
+const Product = () => {
+    return (
+        <div>
+            <Layout>
+                <h1 className="text-2xl text-gray-800 font-light">Products</h1>
+            </Layout>
+        </div>
+    );
+}
+
+export default Product;
