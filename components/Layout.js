cb19193e73ff94b6e7fd2399bcb74c4c8954f071
Richar Urbano <richar.urbano@bizagi.com>
2021-11-30 21:40:42 -0500

    Add support to create user

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
         <>
             <Head>
                 <title>CRM - Administración de Clientes</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
                 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
             </Head>
 
--- pages/newaccount.js
+++ pages/newaccount.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { useMutation, gql } from '@apollo/client';
+
+const NEW_ACCOUNT = gql`
+    mutation NewUser($input: UserInput) {
+        newUser(input: $input) {
+            id
+            firstName
+            lastName
+            email
+        }
+    }
+`;
 
 const NewAccount = () => {
+    // Create new user from GraphQL
+    const [newUser] = useMutation(NEW_ACCOUNT);
+    const [message, setMessage] = useState(null);
+    const router = useRouter();
 
     const formik = useFormik({
         initialValues: {
@@ -24,15 +41,50 @@ const NewAccount = () => {
                 .required("The password is a required field")
                 .min(6, "The passwords field must contain at least 6 characters")
         }),
-        onSubmit: (values) => {
-            console.log("sending...");
-            console.log(values)
+        onSubmit: async (values) => {
+            const { firstName, lastName, email, password } = values;
+
+            try {
+                const { data } = await newUser({
+                    variables: {
+                        input: {
+                            firstName,
+                            lastName,
+                            email,
+                            password
+                        }
+                    }
+                });
+
+                setMessage(`The user ${data.newUser.firstName} was successfully created`);
+
+                setTimeout(() => {
+                    setMessage(null);
+                    router.push("/login");
+                }, 3000);
+
+            } catch (error) {
+                setMessage(error.message);
+
+                setTimeout(() => {
+                    setMessage(null);
+                }, 3000);
+            }
         }
     });
+
+    const showMessage = () => {
+        return (
+            <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+                <p>{message}</p>
+            </div>
+        )
+    };
 
     return (
         <>
             <Layout>
+                {message && showMessage()}
                 <h1 className="text-center text-2xl text-white font-light">Create a New Account</h1>
                 <div className="flex justify-center mt-5">
                     <div className="w-full max-w-sm">
@@ -40,7 +92,9 @@ const NewAccount = () => {
                             className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
                             onSubmit={formik.handleSubmit}>
                             <div className="mb-4">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
+                                <label
+                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                    htmlFor="firstName">
                                     First Name
                                 </label>
                                 <input
@@ -97,7 +151,8 @@ const NewAccount = () => {
                                 </div>
                             ) : null}
                             <div className="mb-4">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                <label
+                                    className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                                     Password
                                 </label>
                                 <input
@@ -110,12 +165,14 @@ const NewAccount = () => {
                                     onBlur={formik.handleBlur} />
                             </div>
                             {formik.touched.password && formik.errors.password ? (
-                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <div
+                                    className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
                                     <p className="font-bold">Error</p>
                                     <p>{formik.errors.password}</p>
                                 </div>
                             ) : null}
-                            <input type="submit"
+                            <input
+                                type="submit"
                                 className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
                                 value="Create Account" />
                         </form>
