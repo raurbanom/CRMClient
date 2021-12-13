8c5e80247af0609e83790b0249c1099c9c3171bd
Richar Urbano <richar.urbano@hotmail.com>
2021-12-12 22:59:33 -0500

    Add support to create a new client

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout'
 import { gql, useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const GET_CLIENTS_USER = gql`
   query GetClientsBySeller {
@@ -30,6 +31,9 @@ const Index = () => {
     < div >
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Clients</h1>
+        <Link href="/newclient">
+          <a className='bg-gray-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-900 mb-3 uppercase'>New Client</a>
+        </Link>
         <table className='table-auto shadow-md mt-10 w-full w-lg'>
           <thead className='bg-gray-800'>
             <tr className='text-white'>
---
+++ pages/newclient.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from "yup";
+import { gql, useMutation } from "@apollo/client";
+import { useRouter } from 'next/router';
+
+const NEW_CLIENT = gql`
+    mutation NewClient($input: ClientInput) {
+        newClient(input: $input) {
+            id
+            firstName
+            lastName
+            company
+            email
+            phone
+        }
+    }
+`;
+
+const NewClient = () => {
+    const router = useRouter();
+
+    const [newClient] = useMutation(NEW_CLIENT);
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: "",
+            lastName: "",
+            company: "",
+            email: "",
+            phone: ""
+        },
+        validationSchema: Yup.object({
+            firstName: Yup.string()
+                .required("The first name is a required field"),
+            lastName: Yup.string()
+                .required("The last name is a required field"),
+            company: Yup.string()
+                .required("The company is a required field"),
+            email: Yup.string()
+                .email("The email address is not valid")
+                .required("The email is a required field")
+        }),
+        onSubmit: async (values) => {
+            const { firstName, lastName, company, email, phone } = values;
+
+            try {
+                const { data } = await newClient({
+                    variables: {
+                        input: {
+                            firstName,
+                            lastName,
+                            company,
+                            email,
+                            phone
+                        }
+                    }
+                });
+
+                router.push("/");
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    });
+
+    return (
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">New Client</h1>
+            <div className='flex justify-center mt-5'>
+                <div className='w-full max-w-lg'>
+                    <form
+                        className='bg-white shadow-md px-8 pt-6 pb-8 mb-4'
+                        onSubmit={formik.handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
+                                First Name
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-1"
+                                id="firstName"
+                                type="text"
+                                placeholder="Client's First Name"
+                                value={formik.values.firstName}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur} />
+                        </div>
+                        {formik.touched.firstName && formik.errors.firstName ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.firstName}</p>
+                            </div>
+                        ) : null}
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
+                                Last Name
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-1"
+                                id="lastName"
+                                type="text"
+                                placeholder="Client's Last Name"
+                                value={formik.values.lastName}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur} />
+                        </div>
+                        {formik.touched.lastName && formik.errors.lastName ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.lastName}</p>
+                            </div>
+                        ) : null}
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="company">
+                                Company
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-1"
+                                id="company"
+                                type="text"
+                                placeholder="Client's Company"
+                                value={formik.values.company}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur} />
+                        </div>
+                        {formik.touched.company && formik.errors.company ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.company}</p>
+                            </div>
+                        ) : null}
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-1"
+                                id="email"
+                                type="email"
+                                placeholder="Client's Email"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur} />
+                        </div>
+                        {formik.touched.email && formik.errors.email ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.email}</p>
+                            </div>
+                        ) : null}
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+                                Phone
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-1"
+                                id="phone"
+                                type="tel"
+                                placeholder="Client's Phone"
+                                value={formik.values.phone}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur} />
+                        </div>
+                        <input
+                            type="submit"
+                            className='bg-gray-800 w-full mt-5 p-2 text-white uppercase font-ball hover:bg-gray-900'
+                            value="Register Client"
+                        />
+                    </form>
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default NewClient;
