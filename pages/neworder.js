1615848155a7c5ac7d367fb8c77276a2ac65cde2
Richar Urbano <richar.urbano@hotmail.com>
2021-12-19 22:57:26 -0500

    Add support to create a new order

--- pages/neworder.js
+++ pages/neworder.js
@@ -1,5 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Layout from '../components/Layout';
+
+import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+import Swal from 'sweetalert2';
 
 import AssignClient from '../components/orders/AssignClient';
 import AssignProducts from '../components/orders/AssignProducts';
@@ -8,11 +12,24 @@ import Total from '../components/orders/Total';
 
 import OrderContext from '../context/orders/OrderContext';
 
+const NEW_ORDER = gql`
+    mutation NewOrder($input: OrderInput) {
+        newOrder(input: $input) {
+            id
+        }
+    }
+`;
+
 const NewOrder = () => {
+
+    const router = useRouter();
+    const [message, setMessage] = useState(null);
 
     // Use context and extract values and functions
     const orderContext = useContext(OrderContext);
     const { client, products, total } = orderContext;
+
+    const [newOrder] = useMutation(NEW_ORDER);
 
     const validateOrder = () => {
         return !products.every((product) => product.quantity > 0) ||
@@ -22,9 +39,49 @@ const NewOrder = () => {
             ""
     };
 
+    const createNewOrder = async () => {
+        const orderDetail = products.map(({ __typename, stock, ...product }) => product);
+
+        try {
+            const { data } = await newOrder({
+                variables: {
+                    input: {
+                        client: client.id,
+                        total,
+                        orderDetail
+                    }
+                }
+            });
+
+            router.push("/orders");
+
+            Swal.fire({
+                title: 'Created!',
+                text: "The order was successfully created",
+                icon: 'success',
+                confirmButtonColor: '#3085d6'
+            });
+        } catch (error) {
+            setMessage(error.message);
+
+            setTimeout(() => {
+                setMessage(null);
+            }, 2000);
+        }
+    };
+
+    const showMessage = () => {
+        return (
+            <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+                <p>{message}</p>
+            </div>
+        )
+    };
+
     return (
         <Layout>
             <h1 className="text-2xl text-gray-800 font-light">New Order</h1>
+            {message && showMessage()}
             <div className='flex justify-center mt-5'>
                 <div className='w-full max-w-lg'>
                     <AssignClient />
@@ -35,6 +92,7 @@ const NewOrder = () => {
                     <button
                         type='button'
                         className={`bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 ${validateOrder()}`}
+                        onClick={() => createNewOrder()}
                     >
                         Register Product
                     </button>
